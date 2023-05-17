fe275d7bcf278489f844b6ebdca9693a36e0cff4
khy0409 <hyeyeong.ijn@gmail.com>
2023-05-17 16:09:59 +0900

    jsjsjsjsjsjsjsjsjsjsjs지멋대로 js

---
+++ cong.js
@@ -0,0 +1,15 @@
+const score = prompt("성적을 입력하세요");
+
+kk = Number(score);
+
+if (Number.isNaN(kk) || kk < 0) {
+  console.log("ERROR");
+} else if (kk >= 80) {
+  console.log("A");
+} else if (60 <= kk) {
+  console.log("B");
+} else if (40 <= kk) {
+  console.log("c");
+} else {
+  console.log("F");
+}
